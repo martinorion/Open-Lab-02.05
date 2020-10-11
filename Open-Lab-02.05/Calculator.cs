6794da164e140670f17acfba6cfb157429acae8a
martinorion <71751917+martinorion@users.noreply.github.com>
2020-10-11 14:09:32 +0200

    If the number is divisible by 5 return true

--- Open-Lab-02.05/Calculator.cs
+++ Open-Lab-02.05/Calculator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_02._05
 {
@@ -6,7 +6,11 @@ namespace Open_Lab_02._05
     {
         public bool DivisibleByFive(int number)
         {
-            throw new NotImplementedException();
+            if (number % 5 == 0)
+                return true;
+            else
+                return false;
         }
     }
 }
+
